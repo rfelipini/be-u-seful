41879f5225e0b532cadbaf96f842e556569e16a4
eduardo <eduardo.brandiao.adm@gmail.com>
2021-11-30 16:51:16 -0300

    create done

--- app/controllers/volunteers_controller.rb
+++ app/controllers/volunteers_controller.rb
@@ -1,5 +1,29 @@
 class VolunteersController < ApplicationController
+
   def new
     @volunteer = Volunteer.new
+    #authorize @volunteer
   end
+
+  def show
+  end
+  def create
+    @volunteer = Volunteer.new(volunteer_params)
+    @volunteer.user = current_user
+    #authorize @volunteer
+    @volunteer.save
+    if @volunteer.save
+      redirect_to @volunteer, notice: 'Volunteer was successfully created.'
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def volunteer_params
+    params.require(:volunteer).permit(:full_name, :github, :background)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  resources :volunteers, only: [:new, :create]
-  resources :students, only: [:new, :create]
+  resources :volunteers, only: [:new, :create, :show]
+  resources :students, only: [:new, :create, :show]
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
