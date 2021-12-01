a9467c916dea00070b6aa0e7a0fe4286258ccb50
eduardo <eduardo.brandiao.adm@gmail.com>
2021-12-01 15:45:00 -0300

    done professional experience and course

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   resources :volunteers, only: [:new, :create, :show]
   resources :students, only: [:new, :create, :show] do
     resources :fields_of_interests, only: [:new, :create]
+    resources :professional_experiences, only: [:new, :create]
+    resources :courses, only: [:new, :create]
   end
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
