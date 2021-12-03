28b35fde6d24728c881c29fd116137f6551c5f17
Raphael Felipini <raphaelfelipini@hotmail.com>
2021-12-03 20:16:19 +0100

    landing new

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,15 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home, :sejavoce ]
 
   def home
+    @background_color = "#E5285E"
+  end
+
+  def seja_voce
+    @background_color = "#480ca8"
+  end
+
+  def seja_util
+    @background_color = "#218380"
   end
 end
